882729ce4440a6e739b48e2ed26b2c1c5377dcd4
SAKTHI S <sakthi138540@gmail.com>
2025-12-29 15:18:00 +0530

    Remove config.py from tracking and ignore secrets

--- Backend/config.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import URL
-
-DATABASE_URL = URL.create(
-    "mysql+pymysql",
-    username = 'sakthi',
-    password = 'Strong@$@Kth!@138540',
-    host = 'localhost',
-    database = 'value_addition_db'
-)
